7a1da0bf161eb5f671a3850f772b95316e3a8986
Spencer Kinney <spencer_kinney@yahoo.com>
2024-11-28 23:01:08 -0600

    New folder structure for python & typescript

---
+++ python/src/aicontext/__init__.py
@@ -0,0 +1,5 @@
+# src/aicontext/__init__.py
+from .core import AIChatContext, AIChatMessage, ai_chat_context
+
+__version__ = "0.0.1"
+__all__ = ["AIChatContext", "AIChatMessage", "ai_chat_context"]
